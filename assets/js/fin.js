25bd26789ad0e69a5dda2aeaa47de1d6ccb86918
Carlos <caragomezher@unal.edu.co>
2016-11-22 22:01:20 +0000

    dogWorld

--- assets/js/fin.js
+++ assets/js/fin.js
@@ -3,15 +3,15 @@
 var winState = {
 
     preload: function(){
-         saveWorld.load.image('findeljuego', 'assets/img/game/fin.png');
+         dogWorld.load.image('findeljuego', 'assets/img/game/fin.png');
     },
 
     create: function () {
         // Agregar fondo
-        saveWorld.add.sprite(0, 0, 'findeljuego');
+        dogWorld.add.sprite(0, 0, 'findeljuego');
 
         // teclaPresionada guarda la acción de presionar R para reanudar el juego
-        var teclaPresionada = saveWorld.input.keyboard.addKey(Phaser.Keyboard.R);
+        var teclaPresionada = dogWorld.input.keyboard.addKey(Phaser.Keyboard.R);
         
         // Cuando presiona se cambia a esa acción
         teclaPresionada.onDown.addOnce(this.start, this);
@@ -24,6 +24,6 @@ var winState = {
 
     // reinicia el juego   
     start: function () {
-        saveWorld.state.start('juego');    
+        dogWorld.state.start('juego');    
     }	
 }
