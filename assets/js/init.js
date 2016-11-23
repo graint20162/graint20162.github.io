0f03aeec7e33ba8f3cae83e03df2705834d1f2a2
Carlos Gómez <caragomezher@unal.edu.co>
2016-11-23 11:36:41 -0500

    Update init.js

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,9 +1,9 @@
 // Se crea el lienzo donde se carga el juego
-var dogWorld = new Phaser.Game(800, 300, Phaser.AUTO, 'destruye');
+var dogWorld = new Phaser.Game(800, 300, Phaser.AUTO, 'entrenandoMascota');
 
 // Se crean los estados del juego
 dogWorld.state.add('juego',iniciar);
-dogWorld.state.add('fin', winState);
+dogWorld.state.add('fin', estadoGanador);
 
 // Se llama al primer estado
 dogWorld.state.start('juego');
