4f53dab93e630ed003db2fd039312c458a6172eb
Carlos Gómez <caragomezher@unal.edu.co>
2016-11-23 14:14:58 -0500

    Update init.js

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,3 +1,13 @@
+/* ********************************
+   ********************************
+Presentado por: Jhonn Alejandro Rojas Robayo
+Codigo: 25571143
+Correo: jharojasro@unal.edu.co
+Universidad Nacional de Colombia
+
+************************************
+******************************** */
+
 // Se crea el lienzo donde se carga el juego
 var dogWorld = new Phaser.Game(800, 300, Phaser.AUTO, 'entrenandoMascota');
 
